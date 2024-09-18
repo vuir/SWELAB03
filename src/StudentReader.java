2b90fe4b20651f2831fd6c23eff03919e4418e95
vuir <Taevio@hotmail.com>
2024-09-18 20:38:36 +0300

    randomizerEdit

--- src/StudentReader.java
+++ src/StudentReader.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 public class StudentReader {
     ArrayList<String> studentNames = new ArrayList<>();
  
-    // Method to add student names
     public void addStudentNames() {
         studentNames.add("Amal");
         studentNames.add("Jood");
@@ -15,6 +14,18 @@ public class StudentReader {
         System.out.println("Student list:");
         for (String name : studentNames) {
             System.out.println(name);
+        }
+    }
+ 
+    public void createRandomGroups() {
+        for (int i = 0; i < studentNames.size(); i += 2) {
+            if (i + 1 < studentNames.size()) {
+                // Print a pair
+                System.out.println("Group: " + studentNames.get(i) + ", " + studentNames.get(i + 1));
+            } else {
+                // If odd number of students, the last group will have 3 students
+                System.out.println("Group: " + studentNames.get(i - 1) + ", " + studentNames.get(i));
+            }
         }
     }
 }
